4d6c672f1f61eb0317ee6dfff44866a2df5b6f69
Vojtech Skrlik <vojtech.skrlik@email.com>
2024-08-02 14:53:26 +0200

    changed app name

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Fibbonaci calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
